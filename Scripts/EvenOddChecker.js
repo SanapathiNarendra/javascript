322fa337e8466b944ab9ceec0a9cb258c3c87044
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-14 14:10:59 +0530

    Create  a EvenOdd checker

---
+++ Scripts/EvenOddChecker.js
@@ -0,0 +1,11 @@
+function onClickCheck() {
+    debugger;
+    var value = parseInt(document.getElementById("txtValue").value);
+    if (value % 2 == 0) {
+        document.getElementById("pResult").innerHTML = "Result: Even Number";
+    }  
+     else 
+    {
+        document.getElementById("pResult").innerHTML = "Result: Odd Number";
+    }   
+}
