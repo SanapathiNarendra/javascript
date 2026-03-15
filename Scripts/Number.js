2e8c2571712c6057c3bd2ae5471507b99e40d935
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-15 15:34:21 +0530

    Feat(Number):Add a Number values

--- Scripts/Number.js
+++ Scripts/Number.js
@@ -3,3 +3,11 @@ function onClickNumberChecker (){
     var result= Number(document.getElementById("txtValue").value);
       document.getElementById("pResult").innerHTML=result;
 }
+ document.getElementById("demo").innerHTML = 
+  Number("-10") + "<br>" +
+  Number("-10.33") + "<br>" +
+  Number("10") + "<br>" +
+  Number("10.33") + "<br>" +
+  Number("10 6") + "<br>" +  
+  Number("10 years") + "<br>" +  
+  Number("years 10");  
