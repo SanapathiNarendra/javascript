95425a30f3b85c66f62096141dc7c2008516f53e
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-15 16:42:52 +0530

    Fix(ToFixed):Create a File ToFixed

--- Scripts/Number.js
+++ Scripts/Number.js
@@ -1,13 +1,15 @@
-function onClickNumberChecker (){
+function onClickNumberfunction(){
     debugger;
     var result= Number(document.getElementById("txtValue").value);
       document.getElementById("pResult").innerHTML=result;
 }
- document.getElementById("demo").innerHTML = 
+ document.getElementById("pDemo").innerHTML = 
   Number("-10") + "<br>" +
   Number("-10.33") + "<br>" +
   Number("10") + "<br>" +
   Number("10.33") + "<br>" +
   Number("10 6") + "<br>" +  
   Number("10 years") + "<br>" +  
-  Number("years 10");  
+  Number("years 10") + "<br>" +
+  Number (true) +  "<br>"  + 
+  Number (false);
---
+++ Scripts/ToFixed.js
@@ -0,0 +1,7 @@
+function onClickToFixed (){
+   debugger;
+   var decimalValue = Number(document.getElementById("txtDecimalValue").value);
+   var fixedValue = Number(document.getElementById("txtFixedValue").value);
+
+   document.getElementById("pResult").innerHTML = decimalValue.toFixed(fixedValue);
+}
