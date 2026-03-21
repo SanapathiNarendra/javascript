09c7c3216d9b7ffaa44e6a61fefe7709d2ce9ab7
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-21 16:12:02 +0530

    Feat(FiniteChecker):implimentation of function in js

--- FiniteChecker.js
+++ FiniteChecker.js
@@ -4,6 +4,10 @@ function OnclickFiniteChecker(){
    var inputsecondvalue=document.getElementById("txtsecondValue").Value;
    var numericvalue=parseInt(inputfirstvalue);
     var numericvalue=parseInt(inputsecondvalue);
+    var divisionResult=1/numericvalue;
+
+    var result = isFinite(divisionResult);
+    document.getElementById("result").innerHTML
     
 
    document.getElementsById("")
