3c00315aa79f6278bad1f96ec7fcd25c66afe27c
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-21 16:47:13 +0530

    Feat(FiniteChecker):Implimentation of the function

--- Scripts/FiniteChecker.js
+++ Scripts/FiniteChecker.js
@@ -1,11 +1,11 @@
 function OnclickFiniteChecker(){
    debugger;
-   var inputfirstvalue=document.getElementById("txtfirstValue").Value;
-   var inputsecondvalue=document.getElementById("txtsecondValue").Value;
+   var inputfirstvalue =(document.getElementById("txtfirstValue").value); 
+   var inputsecondvalue=document.getElementById("txtsecondValue").value;
    var numericvalue=parseInt(inputfirstvalue);
     var numericvalue=parseInt(inputsecondvalue);
     var divisionResult=1/numericvalue;
 
     var result = isFinite(divisionResult);
-    document.getElementById("result").innerHTML="Is the Finite value" + result ;
+    
 }
