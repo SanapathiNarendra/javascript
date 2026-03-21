f0e96b683872a598f56d925f1b8f57b441f19ab4
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-21 16:03:55 +0530

    Feat(FiniteChecker): Add  a  input second Value

--- FiniteChecker.js
+++ FiniteChecker.js
@@ -3,6 +3,7 @@ function OnclickFiniteChecker(){
    var inputfirstvalue=document.getElementById("txtfirstValue").Value;
    var inputsecondvalue=document.getElementById("txtsecondValue").Value;
    var numericvalue=parseInt(inputfirstvalue);
+    var numericvalue=parseInt(inputsecondvalue);
     
 
    document.getElementsById("")
