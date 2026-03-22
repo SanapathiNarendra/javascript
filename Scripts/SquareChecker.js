f5d9bcae93ad193d1399b791a58f7cb1f87f3ded
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-22 12:55:28 +0530

    Feat(SqaureChecker):Implimentation of the javaScript function

---
+++ Scripts/SquareChecker.js
@@ -0,0 +1,7 @@
+function OnclickSquareChecker(){
+ debugger;
+ let Var1 = Number(document.getElementById("txtsquareChecker").value);
+  let Var2 = Number(document.getElementById("txtsquarecheck").value);
+     
+    document.getElementById("pResult").innerHTML = Var1 * Var2 ;
+}
