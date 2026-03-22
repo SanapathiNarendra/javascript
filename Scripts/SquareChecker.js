a6725a1b9031aaa01fd91b708c3a9948a7eafcc7
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-22 17:53:10 +0530

    Feat(squarechecker):Link the function to HTML

--- Scripts/SquareChecker.js
+++ Scripts/SquareChecker.js
@@ -1,7 +1,5 @@
 function OnclickSquareChecker(){
  debugger;
- let Var1 = Number(document.getElementById("txtsquareChecker").value);
-  let Var2 = Number(document.getElementById("txtsquarecheck").value);
-     
-    document.getElementById("pResult").innerHTML = Var1 * Var2 ;
+  var InputValue=document.getElementById("txtInputValue").value;
+    document.getElementById("pResult").innerHTML=Math.sqrt(InputValue);
 }
