109a54e901dc095414b93f516a845b74d198a95e
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-22 18:15:36 +0530

    Feat(Mathpow):Add a variable and input value

--- Scripts/MathPower.js
+++ Scripts/MathPower.js
@@ -1,4 +1,5 @@
 function OnclickMathChecker(){
   debugger;
-  
+   var InputValue=document.getElementById("txtInputValue").value;
+    document.getElementById("pResult").innerHTML=Math.sqrt(InputValue);
 }
