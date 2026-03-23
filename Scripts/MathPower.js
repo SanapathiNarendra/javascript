4346c7f7b6ff245b9601e0cd61170eb639540d3a
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-23 19:42:49 +0530

    Feat(MathPower):Crate a Variable in function

--- Scripts/MathPower.js
+++ Scripts/MathPower.js
@@ -2,5 +2,6 @@ function onclickMathPower(){
  debugger;
   var Value=document.getElementById("txtmathValue").value;
   var Power=document.getElementById("txtmathPower").value;
+  Var Cube=document.getElementById("txt")
     document.getElementById("pResult").innerHTML=Math.pow(Power , Value);
 }
