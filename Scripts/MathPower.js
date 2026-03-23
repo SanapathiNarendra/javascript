99c21654101468eb307ef4a18350afed34099c77
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-23 19:46:48 +0530

    Feat(MathPower): Remove the variable

--- Scripts/MathPower.js
+++ Scripts/MathPower.js
@@ -2,6 +2,5 @@ function onclickMathPower(){
  debugger;
   var Value=document.getElementById("txtmathValue").value;
   var Power=document.getElementById("txtmathPower").value;
-  Var Cube=document.getElementById("txt")
     document.getElementById("pResult").innerHTML=Math.pow(Power , Value);
 }
