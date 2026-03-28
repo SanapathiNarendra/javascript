62e51c8ca1c9ae8aecee7469233bc35d98223eec
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-28 13:22:08 +0530

    Feat(MathMin):Add a variables in js

--- Scripts/Scripts/MathMin.js
+++ Scripts/Scripts/MathMin.js
@@ -1,4 +1,12 @@
 function onClickFindMinValue(){
-    debugger;
+      debugger;
+    var digits=Number(document.getElementById("txtDigits").value);
+    var value1=Math.pow(10,digits)+ Math.random();
+    var value2=Math.pow(10,digits)+ Math.random();
+    var value3=Math.pow(10,digits)+ Math.random();
+    var value4=Math.pow(10,digits)+ Math.random();
+    var value5=Math.pow(10,digits)+ Math.random();
+    var minNumber = Math.min(value1,value2,value3,value4,value5);
+    document.getElementById("pResult").innerHTML=minNumber;
 
 }
