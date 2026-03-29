5f533e3ffd542d9ed91ec3a8401c95e1b30aee90
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-29 15:26:08 +0530

    Feat(IfElse):Add if function in js

--- Scripts/Ifelse.js
+++ Scripts/Ifelse.js
@@ -1,4 +1,10 @@
 function onclickIfElseValue(){
     debugger;
-   
+   var inputNumber=Number(document.getElementById("txtIfelse").value);
+    if ((inputNumber %3 == 0) && (inputNumber %7 == 0)){
+         document.getElementById("pResult").innerHTML=inputNumber+100;
+         
+
+}
+
 }
