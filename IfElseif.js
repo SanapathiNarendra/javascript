078a3145758a705e510b7e4af853fa7ba1d0fd21
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-29 15:56:10 +0530

    Feat(Ifelseif):Link Html code and js code

--- IfElseif.js
+++ IfElseif.js
@@ -3,15 +3,15 @@ function onclickIfelseif(){
    var inputNumber=Number(document.getElementById("txtIfelseif").value);
    if ((inputNumber %3 == 0) && (inputNumber %7 == 0)){
          document.getElementById("pResult").innerHTML="Number is divisible by 3 and 7" ;
+         console.log("Number is divisible by 3 and 7")
     }
     else  if((inputNumber %3==0) ||(inputNumber %7==0)){
      document.getElementById("pResult").innerHTML= "This Number is divisible by 3 or 7";
+        console.log("Number is divisible by 3 or 7")
     }  
    else {
     document.getElementById("pResult").innerHTML= "This Number is not divisible by 3 or 7";
     console.log("This Number is not divisible by 3 or 7")
 }  
-
-
 
 }
