d9a78325c54b062684a6e863673521403bd56d4d
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-29 15:52:45 +0530

    Feat(Ifelseif):Add a Else condition of js

--- IfElseif.js
+++ IfElseif.js
@@ -2,9 +2,15 @@ function onclickIfelseif(){
    debugger; 
    var inputNumber=Number(document.getElementById("txtIfelseif").value);
    if ((inputNumber %3 == 0) && (inputNumber %7 == 0)){
-         document.getElementById("pResult").innerHTML=inputNumber+100;
+         document.getElementById("pResult").innerHTML="Number is divisible by 3 and 7" ;
     }
-    
+    else  if((inputNumber %3==0) ||(inputNumber %7==0)){
+     document.getElementById("pResult").innerHTML= "This Number is divisible by 3 or 7";
+    }  
+   else {
+    document.getElementById("pResult").innerHTML= "This Number is not divisible by 3 or 7";
+    console.log("This Number is not divisible by 3 or 7")
+}  
 
 
 
