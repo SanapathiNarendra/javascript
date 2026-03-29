10c868032a3c2bbf52ffa276b9f7f8dbfc83609a
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-29 15:41:44 +0530

    Feat(IfElseif):Impliment the if function in js

--- IfElseif.js
+++ IfElseif.js
@@ -1,5 +1,11 @@
 function onclickIfelseif(){
    debugger; 
    var inputNumber=Number(document.getElementById("txtIfelseif").value);
-   
+   if ((inputNumber %3 == 0) && (inputNumber %7 == 0)){
+         document.getElementById("pResult").innerHTML=inputNumber+100;
+    }
+    
+
+
+
 }
