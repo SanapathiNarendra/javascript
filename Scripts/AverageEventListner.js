5ccde43b01f8b48c06337c82b22ac4016e4bf1ae
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-02 17:55:53 +0530

    Feat(NumberEventListener):Impliment the function in js

---
+++ Scripts/AverageEventListner.js
@@ -0,0 +1,8 @@
+function onClickCalculate() {
+    var value1 = parseInt(document.getElementById("txtValue1").value);
+    var value2 = parseInt(document.getElementById("txtValue2").value);
+    var value3 = parseInt(document.getElementById("txtValue3").value);
+
+    var average = (value1 + value2 + value3) / 3;
+    document.getElementById("pResult").innerHTML = "Average: " + average.toFixed(2);
+}
