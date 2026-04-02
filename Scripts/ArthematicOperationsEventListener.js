81e4c6eb6aabbff29ef8192252fa246a9b29fbcb
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-02 12:47:20 +0530

    Feat(ArithmaticOperations):Add a EventListener

---
+++ Scripts/ArthematicOperationsEventListener.js
@@ -0,0 +1,34 @@
+         document.getElementById("buttonAdd").addEventListener("click",function() {
+         document.getElementById("buttonSub").addEventListener("click",function() {
+         document.getElementById("buttonMultiply").addEventListener("click",function() {
+         document.getElementById("buttonDiv").addEventListener("click",function() {              
+         debugger;              
+                       
+                              
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 + value2;
+                                document.getElementById("pResult").innerHTML = result
+                        });
+                         
+                                debugger;
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 - value2;
+                                document.getElementById("pResult").innerHTML = result
+                        
+                });
+                                debugger;
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 * value2;
+                                document.getElementById("pResult").innerHTML = result
+        });
+                                                debugger;
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                 var value2 = parseInt(document.getElementById("txtValue2").value);
+                                 result= value1/value2;
+                                  document.getElementById("pResult").innerHTML= result
+                        
+               } );
+    
