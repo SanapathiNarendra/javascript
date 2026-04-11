f22ad795bbfffe41361e0cc7a5c5fc4967dd7cd2
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-11 17:17:56 +0530

    Feat(MathPower):Change the values in MathPower

--- Scripts/MathPower.js
+++ Scripts/MathPower.js
@@ -1,6 +1,8 @@
 function onclickMathPower(){
  debugger;
+ 
   var Value=document.getElementById("txtmathValue").value;
   var Power=document.getElementById("txtmathPower").value;
     document.getElementById("pResult").innerHTML=Math.pow(Power , Value);
+
 }
