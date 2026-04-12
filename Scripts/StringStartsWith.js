3ed3b22e8e171122f6f65e6e2034845b7eed384b
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-12 16:56:12 +0530

    Feat(StringEndsWith):Impliment the js code

--- Scripts/StringStartsWith.js
+++ Scripts/StringStartsWith.js
@@ -1,6 +1,7 @@
 function OnclickStringStartsWith() {
-    
+
     var stringName = document.getElementById("txtStringName").value;
     var search = document.getElementById("txtSearch").value;
-    document.getElementById("pResult").innerHTML =stringName.startsWith(search);    
+  document.getElementById("pResult").innerHTML =stringName.startsWith(search);    
+
 }
