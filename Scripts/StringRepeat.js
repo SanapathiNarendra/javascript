c3769f826fa345a858a42358daa70521fbe5ba96
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-13 12:17:46 +0530

    Feat(PdfChecker):Implimentation of html code

--- Scripts/StringRepeat.js
+++ Scripts/StringRepeat.js
@@ -2,6 +2,5 @@ function OnClickRepeat() {
     debugger;
     var stringName = document.getElementById("txtStringRepeat").value;
     var Search = stringName.repeat(3);
-
-    document.getElementById("pResult").innerHTML = Search;
+document.getElementById("pResult").innerHTML = Search;
 }
