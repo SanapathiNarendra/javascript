1cdcdc72bb82b200e1a21e542a1cc0167dececa7
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-21 20:24:00 +0530

    Feat(StringForLoop):Change the element

--- Scripts/StringForLoop.js
+++ Scripts/StringForLoop.js
@@ -1,11 +1,14 @@
 var wordsOfA = [];
 var words = [];
+var wordsOfB=[];
+var wordsOfC=[];
 document.getElementById("btnFind").addEventListener("click", function () {
     debugger;
 
     var ContentA = "";
     var ContentOfWord = "";
     var ContentB="";
+    var ContentC="";
     var input = document.getElementById("txtWord").value;
 
     if (input.toLowerCase().startsWith("a")) {
@@ -30,13 +33,17 @@ document.getElementById("btnFind").addEventListener("click", function () {
     ContentOfWord = ContentOfWord + "<p>" + (j + 1) + "" + words[j] + "<p><br>"
     }
 
-    for (var k = 0; j < words.length; k++) {
+    for (var k = 0; k < words.length; k++) {
 
     ContentB = ContentB + "<p>" + (k + 1) + "" + words[k] + "<p><br>"
+    }
+    for (var l =0; l<words.length; l++){
+        ContentC =ContentC +"<p>" +(l + 1) + "" + words[l] +"<p><br>"
     }
 
     document.getElementById("ResultA").innerHTML = ContentA;
     document.getElementById("ResultO").innerHTML = ContentOfWord;
     document.getElementById("ResultB").innerHTML =ContentB ;
+    document.getElementById("ResultC").innerHTML =
 
 });
