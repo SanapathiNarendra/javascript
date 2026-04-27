6c6c3205ab53ac81a9fdf063c68057147375da9a
Sanapathi_Narendra <sanapathinarendra99@gmail.com>
2026-04-27 20:47:12 +0530

    Update AndOperation.js

--- Scripts/AndOperation.js
+++ Scripts/AndOperation.js
@@ -3,7 +3,8 @@ document.getElementById("btnAndOperation").addEventListener("click",function()
     
 
 debugger;
-   var inputNumber=Number(document.getElementById("txtInputNumber").value);
+
+   var inputNumber=Number(document.getElementById("txtInputNumber").value);<br>
 if ((inputNumber %3 == 0) || (inputNumber %5 == 0)){
         document.getElementById("pResult").innerHTML="The Number is divisible by 3 and 5.";
         console.log("The Number is divisible by 3 and 5.")
