ef299d5cbc8b5f3eacade6fe844b505813665223
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-30 13:00:07 +0530

    Feat(MutableConcepts):Implement the Html function

---
+++ Scripts/MutableConcepts.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("btnCheckdigits").addEventListener("click", function () {
+
+    debugger;
+    
+
+
+var numbers= [10,20,30,40,50]
+  var index= document.getElementById("txtDigits").value;
+  var changeNumber = document.getElementById("txtChangeNumber").value;
+
+ numbers[index] = changeNumber ;
+ document.getElementById("pResult").innerHTML= numbers;
+});
+
+
