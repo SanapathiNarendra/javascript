7d01cc402d38b551efd2102816dd5700257d09ef
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-04-30 17:21:10 +0530

    Feat(ForLoopMath):Implement the  js code

---
+++ Scripts/ForLoopMath.js
@@ -0,0 +1,53 @@
+document.getElementById("btnSubmit").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i*i)
+    }
+});
+
+document.getElementById("btnEven").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i*i)
+    }
+});
+
+document.getElementById("btnOdd").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i)
+    }
+});
+
+document.getElementById("btnCube").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i*i)
+    }
+});
+
+document.getElementById("btnSquare").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i*i)
+    }
+});
+
+document.getElementById("btnNumberPrinter").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtNumber").value);
+    for (i=1; i<=number; i++){
+debugger;
+console.log(i*i*i)
+    }
+});
