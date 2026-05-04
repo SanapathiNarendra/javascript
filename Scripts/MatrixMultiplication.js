633bf2d04ac03b18fab746d29b7402b23c51fe30
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-04 18:22:53 +0530

    Feat(MatrixMultiplication):Implement the js function

--- Scripts/MatrixMultiplication.js
+++ Scripts/MatrixMultiplication.js
@@ -1,14 +1,20 @@
 
 document.getElementById("btnMatrix").addEventListener("click",function(){
     debugger;
- var matrixA=[[1,2,3],[4,5,6],[7,8,9]
-              [11,12,13],[14,15,16],[17,18,19]];
-    
-for(var i=0;i<matrixA.length;i++){
-    for(var j=0;j<matrixA[i].length;j++){
-        console.log(matrixA[i][j]);
-    }                                                                                                                             
- 
+ var matrixA=[[1,2,3],[4,5,6],[7,8,9]];
+ var matrixB=[[11,12,13],[14,15,16],[17,18,19]];
+ var result=[];
+
+ for(var i=0;i<matrixA.length;i++){     
+
+     result[i]=[];
+     for(var j=0;j<matrixA[i].length;j++){
+        result[i][j]=0;
+        for(var k=0;k<matrixA[i].length;k++){
+            result[i][j]+=matrixA[i][k]*matrixB[k][j];
+        }
+    }
+ }
 console.log(result);
 
 });
