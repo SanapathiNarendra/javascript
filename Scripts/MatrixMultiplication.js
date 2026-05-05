d60225927ab8a0f7ce1e24f89271dd568884771a
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-05 18:27:06 +0530

    Feat(MatrixMultiplication):Add a Array in js

--- Scripts/MatrixMultiplication.js
+++ Scripts/MatrixMultiplication.js
@@ -2,14 +2,14 @@
 document.getElementById("btnMatrix").addEventListener("click", function () {
     debugger;
     var matrix = [[1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]];
+        [4, 5, 6],
+        [7, 8, 9]];
 
       var input = Number(document.getElementById("inputNumber").value);
     var result = " ";
 
     for (var i = 0; i < matrix.length; i++) {
-        
+
         for (var j = 0; j < matrix[i].length; j++) {
             result += matrix[i][j] * input + " "
         }
