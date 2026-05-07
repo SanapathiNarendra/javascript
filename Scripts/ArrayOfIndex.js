5ace2f4df5628ba8cc1534296c2d565405cdd77f
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-07 18:39:43 +0530

    Feat(ArrayOfIndex):Add a Html Code

---
+++ Scripts/ArrayOfIndex.js
@@ -0,0 +1,21 @@
+var cars=[];
+var content="";
+function OnclickArrayOfIndex(){
+    debugger;
+    var input=document.getElementById("txtInput").value;
+
+    cars.push(input);
+
+    for(i=0;i<cars.length;i++){
+        content= content+"<p>"+(i+1)+"."+cars[i]+"</p><br>"  
+      }
+      document.getElementById("pResult").innerHTML=content;
+      content="";
+    document.getElementById("txtInput").value="";
+}
+function onClickSearch(){
+    debugger;
+    var inputBox=document.getElementById("txtSearch").value;
+    document.getElementById("pSearch").innerHTML=
+    `${inputBox} is at ${cars.indexOf(inputBox)}index`;
+}
