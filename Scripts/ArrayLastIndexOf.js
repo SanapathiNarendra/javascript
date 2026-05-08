1909ef359f95dc3e9712b88fb79565621fc1afd5
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-08 18:55:54 +0530

    Feat(ArrayLastIndexOf):Add a variables to Colors

--- Scripts/ArrayLastIndexOf.js
+++ Scripts/ArrayLastIndexOf.js
@@ -1,10 +1,18 @@
+    var colors=["Red","Green","Blue","orange","Red","Maroon","Yellow","Pink","Purple"];
 
+    var array = document.getElementById("arrayResult");
+
+    for(let  i=0;i<=colors.length-1;i++){
+          array.innerHTML += colors[i] + ",";
+    }
 
 function onclickLastIndexOf(){
-    debugger;
-    var colors=["Red","Green","Blue","orange","Red","Maroon","Yellow","Pink","Purple"];
+  
     var input=document.getElementById("txtLastIndexOf").value;
 
     var result=colors.lastIndexOf(input);
+  
     document.getElementById("pResult").innerHTML=result;
+
+    
 }
