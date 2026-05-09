c328220463c5c5cdd5987e778c2a1cbd5b7420a1
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-09 19:21:51 +0530

    Added includes button and renamed input element IDs

--- Scripts/ArrayIncludes.js
+++ Scripts/ArrayIncludes.js
@@ -1,7 +1,8 @@
 let birds = ["Parrot", "Pigeon", "Sparrow", "Crow", "Peacock", "Hen", "Cock", "Duck", "Swan", "Eagle"];
 
-let arrayResult = document.getElementById("showArray");
+let arrayResult = document.getElementById("birdsArrayDisplay");
 
-for(let i=0; i<birds.length-1; i++){
+for (let i = 0; i < birds.length - 1; i++) {
     arrayResult.innerHTML += birds[i] + ", ";
 }
+
