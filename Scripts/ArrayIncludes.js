20f8503f0d456716975e7cf9411dfcdcd04804c4
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-12 20:38:11 +0530

    Feat(ArrayIncludes):Change the result Value

--- Scripts/ArrayIncludes.js
+++ Scripts/ArrayIncludes.js
@@ -10,10 +10,12 @@ function checkBirdAvailability() {
     let enteredBirdName = document.getElementById("txtBirdName").value.trim();
 
     let isBirdAvailable = birds.some(
-        
+
         bird => bird.toLowerCase() === enteredBirdName.toLowerCase()
     );
 
+
+    
     document.getElementById("pIncludesResult").innerHTML =
         "Bird Exists : " + isBirdAvailable;
 }
