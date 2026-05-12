3ce6454ce7e7471ea802f024cb755df53def4b6e
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-12 20:36:30 +0530

    Feat(ArrayIncludes):Implement the variable function

--- Scripts/ArrayIncludes.js
+++ Scripts/ArrayIncludes.js
@@ -10,6 +10,7 @@ function checkBirdAvailability() {
     let enteredBirdName = document.getElementById("txtBirdName").value.trim();
 
     let isBirdAvailable = birds.some(
+        
         bird => bird.toLowerCase() === enteredBirdName.toLowerCase()
     );
 
