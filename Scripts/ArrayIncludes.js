cd0c4d3ea51adebb3ae21a360082183bc813a084
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-13 16:36:12 +0530

    Feat(ArrayIncludes):Change the Variable names

--- Scripts/ArrayIncludes.js
+++ Scripts/ArrayIncludes.js
@@ -6,17 +6,17 @@ birdsArrayDisplay.innerHTML = birds.join(",");
 
 function checkBirdAvailability() {
     
-    
+
 
     let enteredBirdName = document.getElementById("txtBirdName").value.trim();
 
     let isBirdAvailable = birds.some(
+        
 
         bird => bird.toLowerCase() === enteredBirdName.toLowerCase()
     );
 
 
-    
     document.getElementById("pIncludesResult").innerHTML =
         "Bird Exists : " + isBirdAvailable;
 }
