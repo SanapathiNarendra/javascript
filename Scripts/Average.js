bf15950da22e1f4d9759b6ab2973f1600f0c652c
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-16 13:19:41 +0530

    Feat(Average):Implement the js function

--- Scripts/Average.js
+++ Scripts/Average.js
@@ -3,6 +3,7 @@ function onClickCalculate() {
     var value2 = parseInt(document.getElementById("txtValue2").value);
     var value3 = parseInt(document.getElementById("txtValue3").value);
 
+    
     var average = (value1 + value2 + value3) / 3;
     document.getElementById("pResult").innerHTML = "Average: " + average.toFixed(2);
     
