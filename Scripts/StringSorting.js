d47a2a9634eabbcb42f7ca8eefb01ab667ef1b3a
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-17 21:27:05 +0530

    Feat(StringSorting):Add a Sort button in Html

--- Scripts/StringSorting.js
+++ Scripts/StringSorting.js
@@ -9,7 +9,15 @@ function OnclickStringSorting() {
     }
 
     document.getElementById("pResult").innerHTML= content;
-    
-    document.getElementById("txtInputWord").value="";    
 
+    document.getElementById("txtInputWord").value="";  
+}
+
+function sortAlphabeticalOrder(){
+    var content="";
+    alphabeticalOrder.sort();
+    for (i=0; i<alphabeticalOrder.length; i++){
+        content =content+"<p>" + (i+1) +"." +alphabeticalOrder [i] +"</p><br>";
+    }
+    document.getElementById("pResult").innerHTML= content;
 }
