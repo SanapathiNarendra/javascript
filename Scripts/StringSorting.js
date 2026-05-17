e56bd3ad806ec66b0d5c74343809257e2fa04195
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-17 21:19:42 +0530

    Feat(StringSorting):Implement the js function

---
+++ Scripts/StringSorting.js
@@ -0,0 +1,13 @@
+var alphabeticalOrder= [];
+
+function OnclickStringSorting() {
+   var content="";
+    var inputWord= document.getElementById("txtInputWord").value;
+    alphabeticalOrder.push(inputWord);
+    for (i=0; i<alphabeticalOrder.length; i++){
+        content =content+"<p>" + (i+1) +"." +alphabeticalOrder [i] +"</p><br>";
+    }
+    document.getElementById("pResult").innerHTML= content;
+    document.getElementById("txtInputWord").value="";    
+
+}
