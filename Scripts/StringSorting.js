8f502e463adf80e7ccf1bf1342f50ac65c3892a0
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-17 21:20:41 +0530

    Feat(StringSorting):Change the Content of String

--- Scripts/StringSorting.js
+++ Scripts/StringSorting.js
@@ -7,6 +7,7 @@ function OnclickStringSorting() {
     for (i=0; i<alphabeticalOrder.length; i++){
         content =content+"<p>" + (i+1) +"." +alphabeticalOrder [i] +"</p><br>";
     }
+    
     document.getElementById("pResult").innerHTML= content;
     document.getElementById("txtInputWord").value="";    
 
