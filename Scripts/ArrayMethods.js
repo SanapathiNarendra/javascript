3d316df6b60afa691bc5eaad1a2d32116bf02e88
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-20 21:41:52 +0530

    Displayed initial array values on page load

---
+++ Scripts/ArrayMethods.js
@@ -0,0 +1,7 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya", "Kiwi", "Jack fruit"];
+
+displayArray();
+
+function displayArray() {
+    document.getElementById("pResult").innerHTML = fruits;
+}
