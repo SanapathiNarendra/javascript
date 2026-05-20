189b45808cfed79abe350722bd188deb37825898
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-20 21:45:38 +0530

    Added array toString() method with output

--- Scripts/ArrayMethods.js
+++ Scripts/ArrayMethods.js
@@ -1,12 +1,17 @@
-let fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya", "Kiwi", "Jack fruit"];
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya", "Kiwi", "Grapes"];
 
 displayArray();
 
 function displayArray() {
-    document.getElementById("result").innerHTML = fruits;
+    document.getElementById("result").innerHTML = "[" + fruits + "]";
 }
 
 function arrayLength() {
     document.getElementById("pResult").innerHTML =
         "Array Length : " + fruits.length;
+}
+
+function arrayToString() {
+    document.getElementById("pResult").innerHTML =
+        "toString() : " + fruits.toString();
 }
