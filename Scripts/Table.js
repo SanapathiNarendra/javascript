defecc0a4f2692b31d97a75ab4c94fc8bb9bd59c
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-05-26 20:18:18 +0530

    Added multiplication table logic using for loop

---
+++ Scripts/Table.js
@@ -0,0 +1,14 @@
+function ShowTable() {
+
+    var number = Number(document.getElementById("txtNumber").value);
+
+    var output = "";
+
+    for (let i = 1; i <= 10; i++) {
+
+        output = output + number + " x " + i + " = " + (number * i) + "<br>";
+
+    }
+
+    document.getElementById("result").innerHTML = output;
+}
