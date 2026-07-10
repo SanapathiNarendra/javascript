42e5646c768556de60ccf23984aaf87e11a2cc8d
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-07-10 21:19:07 +0530

    Feat(Discount):Implement the Js Code

--- Scripts/Discount.js
+++ Scripts/Discount.js
@@ -1,6 +1,6 @@
 function onClickSellingprice(){
     debugger;
-    
+
     price=(document.getElementById ("txtprice").value);
     discountPercentage=(document.getElementById("txtdiscount").value);
     discount=( discountPercentage/100)*price;
