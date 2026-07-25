f485d175ebc7c337876dfe010a6ecd28c4366336
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-07-25 22:39:34 +0530

    Feat(ToFixed):Implement the js Code

--- Scripts/AverageEventListner.js
+++ Scripts/AverageEventListner.js
@@ -1,8 +1,8 @@
-document.getElementById("")
+document.getElementById(""){
+
 var value1 = parseInt(document.getElementById("txtValue1").value);
     var value2 = parseInt(document.getElementById("txtValue2").value);
     var value3 = parseInt(document.getElementById("txtValue3").value);
-
     var average = (value1 + value2 + value3) / 3;
     document.getElementById("pResult").innerHTML = "Average: " + average.toFixed(2);
 }
