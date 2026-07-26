2d2fdde2e7a0f03e368fb3a12d6f2209bda1e758
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-07-26 20:39:48 +0530

    Feat(ToFixed):Change the Js Code

--- Scripts/ToFixed.js
+++ Scripts/ToFixed.js
@@ -1,6 +1,5 @@
 function onClickToFixed (){
    debugger;
-   
    var decimalValue = Number(document.getElementById("txtDecimalValue").value);
    var fixedValue = Number(document.getElementById("txtFixedValue").value);
    document.getElementById("pResult").innerHTML = decimalValue.toFixed(fixedValue);
